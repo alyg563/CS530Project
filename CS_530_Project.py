0820542d3fe6b36b9e82f9e2df0fe657d2442b7b
Shane Tasker <stasker5003@sdsu.edu>
2021-10-21 16:37:07 -0700

    Coded fall detection program

--- CS_530_Project.py
+++ CS_530_Project.py
@@ -2,29 +2,38 @@
 # Authors:
 # Shane Tasker, Alyssa Garcia, Sawyer Thompson, Alexander Ray
 
-#Import statements
+# Import statements
 import time
+from sense_hat import SenseHat
 
 fallVelocity = 0
-fallen = false
+fallen = False
+sense = SenseHat()
 
-#Main while loop that constantly queries the accelerometer for current velocity,
+
+# Detect and return current acceleration
+def detect_fall():
+    raw = sense.get_accelerometer_raw()
+    acceleration = raw.get("y")  # Store current acceleration
+    return acceleration  # Return value found
+
+
+# Make emergency call based on inputted contact info
+def call(contact_info):
+    x = 5
+    # Make call to contactInfo number or send email to contactInfo email
+
+
+# Main while loop that constantly queries the accelerometer for current velocity,
 # and makes a call if it is determined that a person is falling
+contactInfo = ""  # Store user inputted contact info
 while not fallen:
-    contactInfo = "" #Store user inputted contact info
-    acceleration = detectFall() #Call fall detection function which interfaces with accelerometer
-    if acceleration > 1: # If object or person is falling
-        call(contactInfo) # Make emergency call
-        fallen = true # exit loop
-    #Potentially have small delay in program to not query accelerometer a ridiculous number of times
-    time.sleep(0.1)
+    y_acceleration = detect_fall()  # Call fall detection function which interfaces with accelerometer
+    if y_acceleration > 1:  # If object or person is falling
+        call(contactInfo)  # Make emergency call
+        fallen = True  # exit loop
+        # Potentially have small delay in program to not query accelerometer a ridiculous number of times
+        time.sleep(0.1)
 
-#Detect and return current acceleration
-def detectFall():
-    acceleration = input from accelerometer #Store current acceleration
-    return acceleration #Return value found
 
-#Make emergency call based on inputted contact info
-def call(contactInfo):
-    make call to contactInfo number or send email to contactInfo email
 
